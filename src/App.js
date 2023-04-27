5c9545551ab6bb945ec942b5ee94c7f9a7eb68c2
farimah <farimah.hsni@gmail.com>
2023-04-27 10:27:09 +0430

    add app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes , Route } from "react-router-dom";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
+import Checkouts from "./components/Checkouts";
+import Header from "./components/Header";
+
+import {styles} from "./css/App.moudle.css";
+ import CartContextProvider from "./context/CartContextProvider";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+
+<CartContextProvider>
+       <Header />
+      <Routes>
+
+          <Route path="/" element={<Home/>} />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/checkouts" element={<Checkouts/>} />
+
+
+  
+       </Routes>
+
+       </CartContextProvider>
     </div>
   );
 }
